c1dbebb7a0e879b298b3745c4f12f816ebe47580
Jasbir Kaur <119117104+JasbirKaur071@users.noreply.github.com>
2025-03-25 18:32:06 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const id=1;
+let accountnumber=12345;
+var accountid=23;
+// acountplace="benglore"; cant be declared we have to use any data type also so it will also give error
+
+// id=2; here id cannot be redeclared as it is const so it will give error
+accountnumber=12;
+accountid=334;
+// acountplace="delhi";
+console.table([id, accountnumber, accountid]);
