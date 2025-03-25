0e93854dfb7993c6b0c4f69fa3ee5c6306dd7fb0
Jasbir Kaur <119117104+JasbirKaur071@users.noreply.github.com>
2025-03-25 18:51:12 +0000

    datatypes conversion

---
+++ 01_basics/data_types.js
@@ -0,0 +1,22 @@
+//using the "use strict" will use the newer version of js
+"use strict";
+console.log(3+3);
+console.log("rahul");
+
+/*data type:
+2 types: Primitve and Non primitive
+Primitive are -nnbbssu
+(Number, Null, Boolean, BigInt, Symbol, String, Undefined)
+Non-primitve=Arrays, objects etc
+*/
+let name="Omkar Anand";
+let age=21;
+let islogged=true;
+console.log(typeof(name));
+console.log(typeof(age));
+
+//type conversion
+let score=33;
+let sno=String(score);
+console.log(typeof score);
+console.log(typeof sno);
